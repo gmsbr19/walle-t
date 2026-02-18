c8e79962d3fddb0fe3edbf21337b6b40d5d25f34
Thiago <thiagovidaportela@gmail.com>
2026-02-17 23:41:07 -0300

    feat: add NavSidebar component

---
+++ src/components/NavSidebar.tsx
@@ -0,0 +1,43 @@
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Home, Settings } from "lucide-react"
+
+const NavSidebar = () => {
+    return (
+        <Sidebar>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Navegação</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            <SidebarMenuItem>
+                                <SidebarMenuButton isActive>
+                                    <Home />
+                                    <span>Início (teste)</span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                            <SidebarMenuItem>
+                                <SidebarMenuButton>
+                                    <Settings />
+                                    <span>Configurações (teste)</span>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter />
+        </Sidebar>
+    )
+}
+
+export default NavSidebar
