71993c55f057ba45732f7cafc23a84553634b394
Thiago <thiagovidaportela@gmail.com>
2026-02-27 19:59:58 -0300

    feat: refactor Home component to display grouped transactions with improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,63 @@
 import NavSidebar from "@/components/NavSidebar"
+import {
+    Item,
+    ItemActions,
+    ItemContent,
+    ItemDescription,
+    ItemMedia,
+    ItemTitle,
+} from "@/components/ui/item"
+import { Separator } from "@/components/ui/separator"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { getDailyHistory, getTransactionYearMonths } from "@/services/transactions"
+import { formatCurrency, formatDateLabel } from "@/utils/format"
+import { DollarSign } from "lucide-react"
 import Image from "next/image"
+import React from "react"
 
-export default function Home() {
+export default async function Home() {
+    const groupedTransactions = await getDailyHistory(2026, 2)
+    const yearMonths = await getTransactionYearMonths()
+    const days = Object.keys(groupedTransactions)
+
+    console.log(yearMonths)
+
     return (
         <SidebarProvider>
             <NavSidebar />
-            <main>
-                <SidebarTrigger />
-                <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
-                    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                        <Image
-                            className="dark:invert"
-                            src="/next.svg"
-                            alt="Next.js logo"
-                            width={100}
-                            height={20}
-                            priority
-                        />
-                        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-                            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                                To get started, edit the page.tsx file.
-                            </h1>
-                            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-                                Looking for a starting point or more
-                                instructions? Head over to{" "}
-                                <a
-                                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                                    className="font-medium text-zinc-950 dark:text-zinc-50"
-                                >
-                                    Templates
-                                </a>{" "}
-                                or the{" "}
-                                <a
-                                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                                    className="font-medium text-zinc-950 dark:text-zinc-50"
-                                >
-                                    Learning
-                                </a>{" "}
-                                center.
-                            </p>
-                        </div>
-                        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-                            <a
-                                className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-                                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <Image
-                                    className="dark:invert"
-                                    src="/vercel.svg"
-                                    alt="Vercel logomark"
-                                    width={16}
-                                    height={16}
-                                />
-                                Deploy Now
-                            </a>
-                            <a
-                                className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-                                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Documentation
-                            </a>
-                        </div>
-                    </main>
-                </div>
+            <main className="w-full">
+                {/* <SidebarTrigger /> */}
+                <section className="w-full px-40 flex flex-col gap-4">
+                    {days.map((day) => (
+                        <React.Fragment key={day}>
+                            <div key={day} className="flex flex-col gap-2">
+                                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                                    {formatDateLabel(day)}
+                                </h3>
+                                <div className="flex flex-col gap-2">
+                                    {groupedTransactions[day].map((t) => (
+                                        <Item className="hover:cursor-pointer hover:bg-accent">
+                                            <ItemMedia variant="icon">
+                                                <DollarSign size={24} />
+                                            </ItemMedia>
+                                            <ItemContent>
+                                                <ItemTitle className="leading-7">
+                                                    {t.description}
+                                                </ItemTitle>
+                                            </ItemContent>
+                                            <ItemActions>
+                                                <span className="font-mono">
+                                                    {formatCurrency(t.amount)}
+                                                </span>
+                                            </ItemActions>
+                                        </Item>
+                                    ))}
+                                </div>
+                            </div>
+                            <Separator />
+                        </React.Fragment>
+                    ))}
+                </section>
             </main>
         </SidebarProvider>
     )
